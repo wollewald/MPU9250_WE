c3ee5a459e4e8d30ef757ee5d6a8b75c7b93f08f
Wolfgang (Wolle) Ewald <wolfgang.ewald@wolles-elektronikkiste.de>
2021-04-01 11:55:12 +0200

    Add files via upload

--- examples/MPU9250_gyroscope_data/MPU9250_gyroscope_data.ino
+++ examples/MPU9250_gyroscope_data/MPU9250_gyroscope_data.ino
@@ -1,1 +1,117 @@
+/***************************************************************************
+* Example sketch for the MPU9250_WE library
+*
+* This sketch shows how to obtain gyroscope data from the MPU9250. 
+* 
+* Further information can be (soon) found on:
+* https://wolles-elektronikkiste.de      (German)
+* https://wolles-elektronikkiste.de/en/  (English)
+* 
+***************************************************************************/
 
+#include <MPU9250_WE.h>
+#include <Wire.h>
+
+MPU9250_WE myMPU9250 = MPU9250_WE(0x68);
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin();
+  if(!myMPU9250.init()){
+    Serial.println("MPU9250 does not respond");
+  }
+  else{
+    Serial.println("MPU9250 is connected");
+  }
+
+  /*  The gyroscope data is not zero, even if don't move the MPU9250. 
+   *  To start at zero, you can apply offset values. These are the gyroscope raw values you obtain
+   *  using the +/- 250 degrees/s range. 
+   *  Use either autoOffset or setGyrOffsets, not both.
+   */
+  //myMPU9250.setGyrOffsets(45.0, 145.0, -105.0);
+  
+ /*  You can enable or disable the digital low pass filter (DLPF). If you disable the DLPF, you 
+   *  need to select the bandwidth, which can be either 8800 or 3600 Hz. 8800 Hz has a shorter delay,
+   *  but higher noise level. If DLPF is disabled, the output rate is 32 kHz.
+   *  MPU9250_BW_WO_DLPF_3600 
+   *  MPU9250_BW_WO_DLPF_8800
+   */
+  myMPU9250.enableGyrDLPF();
+  //myMPU9250.disableGyrDLPF(MPU9250_BW_WO_DLPF_8800); // bandwidth without DLPF
+  
+  /*  Digital Low Pass Filter for the gyroscope must be enabled to choose the level. 
+   *  MPU9250_DPLF_0, MPU9250_DPLF_2, ...... MPU9250_DPLF_7 
+   *  
+   *  DLPF    Bandwidth [Hz]   Delay [ms]   Output Rate [kHz]
+   *    0         250            0.97             8
+   *    1         184            2.9              1
+   *    2          92            3.9              1
+   *    3          41            5.9              1
+   *    4          20            9.9              1
+   *    5          10           17.85             1
+   *    6           5           33.48             1
+   *    7        3600            0.17             8
+   *    
+   *    You achieve lowest noise using level 6  
+   */
+  myMPU9250.setGyrDLPF(MPU9250_DLPF_6);
+
+  /*  Sample rate divider divides the output rate of the gyroscope and accelerometer.
+   *  Sample rate = Internal sample rate / (1 + divider) 
+   *  It can only be applied if the corresponding DLPF is enabled and 0<DLPF<7!
+   *  Divider is a number 0...255
+   */
+  myMPU9250.setSampleRateDivider(99);
+
+
+  /*  MPU9250_GYRO_RANGE_250       250 degrees per second (default)
+   *  MPU9250_GYRO_RANGE_500       500 degrees per second
+   *  MPU9250_GYRO_RANGE_1000     1000 degrees per second
+   *  MPU9250_GYRO_RANGE_2000     2000 degrees per second
+   */
+  myMPU9250.setGyrRange(MPU9250_GYRO_RANGE_250);
+  
+  /* sends the MPU9250 to sleep or wakes it up */
+  //myMPU9250.sleep(true);
+
+  /* This is a low power standby mode for the gyro function, which allows quick enabling. 
+   * (see data sheet for further information)
+   */
+  //myMPU9250.enableGyrStandby(true);
+
+  
+  /* You can enable or disable the axes for gyrometer and/or accelerometer measurements.
+   * By default all axes are enabled. Parameters are:  
+   * MPU9250_ENABLE_XYZ  //all axes are enabled (default)
+   * MPU9250_ENABLE_XY0  // X, Y enabled, Z disabled
+   * MPU9250_ENABLE_X0Z   
+   * MPU9250_ENABLE_X00
+   * MPU9250_ENABLE_0YZ
+   * MPU9250_ENABLE_0Y0
+   * MPU9250_ENABLE_00Z
+   * MPU9250_ENABLE_000  // all axes disabled
+   */
+  //myMPU9250.enableGyrAxes(MPU9250_ENABLE_000);  
+}
+
+void loop() {
+  xyzFloat gyrRaw = myMPU9250.getGyrRawValues();
+  xyzFloat gyr = myMPU9250.getGyrValues();
+  
+  Serial.println("Gyroscope raw values (x,y,z):");
+  Serial.print(gyrRaw.x);
+  Serial.print("   ");
+  Serial.print(gyrRaw.y);
+  Serial.print("   ");
+  Serial.println(gyrRaw.z);
+
+  Serial.println("Gyroscope Data in degrees/s (x,y,z):");
+  Serial.print(gyr.x);
+  Serial.print("   ");
+  Serial.print(gyr.y);
+  Serial.print("   ");
+  Serial.println(gyr.z);
+
+  delay(1000);
+}
