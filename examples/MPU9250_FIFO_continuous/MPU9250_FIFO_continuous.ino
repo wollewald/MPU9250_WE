42a952f9c5c6791a326159dc586e36baedef311d
Wolfgang (Wolle) Ewald <wolfgang.ewald@wolles-elektronikkiste.de>
2021-04-06 22:41:49 +0200

    Update MPU9250_FIFO_continuous.ino

--- examples/MPU9250_FIFO_continuous/MPU9250_FIFO_continuous.ino
+++ examples/MPU9250_FIFO_continuous/MPU9250_FIFO_continuous.ino
@@ -9,9 +9,10 @@
 * You define, when writing into Fifo shall stop. In this example the stop is triggered 
 * by a wake-on-motion event.
 * 
-* Further information can be (soon) found on:
-* https://wolles-elektronikkiste.de      (German)
-* https://wolles-elektronikkiste.de/en/  (English)
+* For further information visit my blog:
+*
+* https://wolles-elektronikkiste.de/mpu9250-9-achsen-sensormodul-teil-1  (German)
+* https://wolles-elektronikkiste.de/en/mpu9250-9-axis-sensor-module-part-1  (English)
 * 
 ***************************************************************************/
 
