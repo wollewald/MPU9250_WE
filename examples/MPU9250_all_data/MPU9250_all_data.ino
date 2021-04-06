c0b0bce385e673a88680474f0dc56556dcb0dfda
Wolfgang (Wolle) Ewald <wolfgang.ewald@wolles-elektronikkiste.de>
2021-04-06 22:43:28 +0200

    Update MPU9250_all_data.ino

--- examples/MPU9250_all_data/MPU9250_all_data.ino
+++ examples/MPU9250_all_data/MPU9250_all_data.ino
@@ -5,9 +5,10 @@
 * temperature data from the MPU9250. It might be a bit confusing with all the 
 * settings. Therefore you will also find sketches for the individual data.
 * 
-* Further information can be (soon) found on:
-* https://wolles-elektronikkiste.de      (German)
-* https://wolles-elektronikkiste.de/en/  (English)
+* For further information visit my blog:
+*
+* https://wolles-elektronikkiste.de/mpu9250-9-achsen-sensormodul-teil-1  (German)
+* https://wolles-elektronikkiste.de/en/mpu9250-9-axis-sensor-module-part-1  (English)
 * 
 ***************************************************************************/
 
