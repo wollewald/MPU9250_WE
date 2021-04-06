de5b04dece6e42aefe503a5a5407c109d9b14959
Wolfgang (Wolle) Ewald <wolfgang.ewald@wolles-elektronikkiste.de>
2021-04-06 22:47:04 +0200

    Update MPU9250_pitch_and_roll.ino

--- examples/MPU9250_pitch_and_roll/MPU9250_pitch_and_roll.ino
+++ examples/MPU9250_pitch_and_roll/MPU9250_pitch_and_roll.ino
@@ -4,9 +4,10 @@
 * This sketch shows how to measure pitch and roll angles from the MPU9250. 
 * I have also included the corrected angle method for comparison.  
 * 
-* Further information can be (soon) found on:
-* https://wolles-elektronikkiste.de      (German)
-* https://wolles-elektronikkiste.de/en/  (English)
+* For further information visit my blog:
+*
+* https://wolles-elektronikkiste.de/mpu9250-9-achsen-sensormodul-teil-1  (German)
+* https://wolles-elektronikkiste.de/en/mpu9250-9-axis-sensor-module-part-1  (English)
 * 
 ***************************************************************************/
 
