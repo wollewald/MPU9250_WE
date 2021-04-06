ee1a9cb26a88e83f4af5bbb0295afea74de9f251
Wolfgang (Wolle) Ewald <wolfgang.ewald@wolles-elektronikkiste.de>
2021-04-06 22:44:34 +0200

    Update MPU9250_blank_all_settings.ino

--- examples/MPU9250_blank_all_settings/MPU9250_blank_all_settings.ino
+++ examples/MPU9250_blank_all_settings/MPU9250_blank_all_settings.ino
@@ -4,9 +4,10 @@
 * This sketch can be used as a basis for your own sketches. It contains all
 * settings. 
 * 
-* Further information can be (soon) found on:
-* https://wolles-elektronikkiste.de      (German)
-* https://wolles-elektronikkiste.de/en/  (English)
+* For further information visit my blog:
+*
+* https://wolles-elektronikkiste.de/mpu9250-9-achsen-sensormodul-teil-1  (German)
+* https://wolles-elektronikkiste.de/en/mpu9250-9-axis-sensor-module-part-1  (English)
 * 
 ***************************************************************************/
 
