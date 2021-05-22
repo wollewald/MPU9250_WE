d5493c5f6b7edf8d62c51f547bc9fb96ac6716ec
Wolfgang (Wolle) Ewald <wolfgang.ewald@wolles-elektronikkiste.de>
2021-05-22 21:59:41 +0200

    Add files via upload

--- examples/MPU9250_magnetometer_data/MPU9250_magnetometer_data.ino
+++ examples/MPU9250_magnetometer_data/MPU9250_magnetometer_data.ino
@@ -44,25 +44,20 @@ void setup() {
 
   /* You can choose the following operational modes
    * AK8963_PWR_DOWN            power down (default)
-   * AK8963_TRIGGER_MODE        single shot (manual start needed)
    * AK8963_CONT_MODE_8HZ       continuous at 8Hz sample rate
    * AK8963_CONT_MODE_100HZ     continuous at 100Hz sample rate 
    * 
    * In trigger mode the AK8963 goes into power down after the measurement
    */
-  myMPU9250.setMagOpMode(AK8963_CONT_MODE_8HZ);
+  myMPU9250.setMagOpMode(AK8963_CONT_MODE_100HZ);
   
-  /* In continuous mode you need to wait for the first data to be available (after 
-   * max 9 ms). If you comment the while loop below you will probably obtain zero. 
-   * For the trigger mode I have implemented that the AK8983 will always wait for 
-   * available data. In continuous mode you will read the data which is there at 
-   * the moment. It can be up to 10 ms "old" at 100 Hz or up to 125 ms at 8 Hz.
+  /* In continuous mode you need to wait for the first data to be available. If you 
+   * comment the line below you will probably obtain zero. 
    */
-  while(!myMPU9250.isMagDataReady()){}
+  delay(200);
 }
 
 void loop() {
-  //myMPU9250.startMagMeasurement(); // needed in trigger mode
   xyzFloat magValue = myMPU9250.getMagValues(); // returns magnetic flux density [µT] 
 
   Serial.println("Magnetometer Data in µTesla: ");
