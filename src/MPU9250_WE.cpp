03bb1b66111103cbcec02ba022d7479c034f3f52
Wolfgang (Wolle) Ewald <wolfgang.ewald@wolles-elektronikkiste.de>
2021-08-10 13:20:12 +0200

    Update MPU9250_WE.cpp

--- src/MPU9250_WE.cpp
+++ src/MPU9250_WE.cpp
@@ -819,13 +819,13 @@ xyzFloat MPU9250_WE::readMPU9250xyzValFromFifo(){
     xyzFloat xyzResult = {0.0, 0.0, 0.0};
     MSByte = readMPU9250Register8(MPU9250_FIFO_R_W);
     LSByte = readMPU9250Register8(MPU9250_FIFO_R_W);
-    xyzResult.x = (MSByte<<8) + LSByte;
+    xyzResult.x = ((int16_t)((MSByte<<8) + LSByte)) * 1.0;
     MSByte = readMPU9250Register8(MPU9250_FIFO_R_W);
     LSByte = readMPU9250Register8(MPU9250_FIFO_R_W);
-    xyzResult.y = (MSByte<<8) + LSByte;
+    xyzResult.y = ((int16_t)((MSByte<<8) + LSByte)) * 1.0;
     MSByte = readMPU9250Register8(MPU9250_FIFO_R_W);
     LSByte = readMPU9250Register8(MPU9250_FIFO_R_W);
-    xyzResult.z = (MSByte<<8) + LSByte;
+    xyzResult.z = ((int16_t)((MSByte<<8) + LSByte)) * 1.0;
     return xyzResult; 
 }
 
