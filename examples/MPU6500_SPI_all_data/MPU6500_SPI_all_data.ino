a786ed99b081179f6047ffcba30ba68acf945756
Wolfgang (Wolle) Ewald <wolfgang.ewald@wolles-elektronikkiste.de>
2022-03-25 21:48:40 +0100

    Add files via upload

--- examples/MPU6500_SPI_all_data/MPU6500_SPI_all_data.ino
+++ examples/MPU6500_SPI_all_data/MPU6500_SPI_all_data.ino
@@ -1,1 +1,83 @@
+/***************************************************************************
+* Example sketch for the MPU6500_WE library
+*
+* This sketch shows how to get acceleration, gyroscocope and temperature 
+* data from the MPU6500 using SPI.
+* 
+* For further information visit my blog:
+*
+* https://wolles-elektronikkiste.de/mpu9250-9-achsen-sensormodul-teil-1  (German)
+* https://wolles-elektronikkiste.de/en/mpu9250-9-axis-sensor-module-part-1  (English)
+* 
+***************************************************************************/
 
+#include <MPU6500_WE.h>
+const int csPin = 10;  // Chip Select Pin
+bool useSPI = true;    // SPI use flag
+
+
+/* There is only one construictor for SPI: */
+MPU6500_WE myMPU6500 = MPU6500_WE(&SPI, csPin, useSPI);
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin();
+  if(!myMPU6500.init()){
+    Serial.println("MPU6500 does not respond");
+  }
+  else{
+    Serial.println("MPU6500 is connected");
+  }
+
+  /* Choose the SPI clock speed, default is 8 MHz 
+     This function must be used only after init(), not before */
+  //myMPU9250.setSPIClockSpeed(4000000);
+
+  Serial.println("Position you MPU6500 flat and don't move it - calibrating...");
+  delay(1000);
+  myMPU6500.autoOffsets();
+  Serial.println("Done!");
+    
+  //myMPU6500.setAccOffsets(-14240.0, 18220.0, -17280.0, 15590.0, -20930.0, 12080.0);
+  //myMPU6500.setGyrOffsets(45.0, 145.0, -105.0);
+  myMPU6500.enableGyrDLPF();
+  //myMPU6500.disableGyrDLPF(MPU6500_BW_WO_DLPF_8800); // bandwdith without DLPF
+  myMPU6500.setGyrDLPF(MPU6500_DLPF_6);
+  myMPU6500.setSampleRateDivider(5);
+  myMPU6500.setGyrRange(MPU6500_GYRO_RANGE_250);
+  myMPU6500.setAccRange(MPU6500_ACC_RANGE_2G);
+  myMPU6500.enableAccDLPF(true);
+  myMPU6500.setAccDLPF(MPU6500_DLPF_6);
+  //myMPU6500.enableAccAxes(MPU6500_ENABLE_XYZ);
+  //myMPU6500.enableGyrAxes(MPU6500_ENABLE_XYZ);
+}
+
+void loop() {
+  xyzFloat gValue = myMPU6500.getGValues();
+  xyzFloat gyr = myMPU6500.getGyrValues();
+  float temp = myMPU6500.getTemperature();
+  float resultantG = myMPU6500.getResultantG(gValue);
+
+  Serial.println("Acceleration in g (x,y,z):");
+  Serial.print(gValue.x);
+  Serial.print("   ");
+  Serial.print(gValue.y);
+  Serial.print("   ");
+  Serial.println(gValue.z);
+  Serial.print("Resultant g: ");
+  Serial.println(resultantG);
+
+  Serial.println("Gyroscope data in degrees/s: ");
+  Serial.print(gyr.x);
+  Serial.print("   ");
+  Serial.print(gyr.y);
+  Serial.print("   ");
+  Serial.println(gyr.z);
+
+  Serial.print("Temperature in °C: ");
+  Serial.println(temp);
+
+  Serial.println("********************************************");
+
+  delay(1000);
+}
