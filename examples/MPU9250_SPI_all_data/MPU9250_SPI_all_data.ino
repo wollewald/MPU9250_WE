a0e4571dcd22d8b5c4d627a2ae6d04a91fc8ab4c
Wolfgang (Wolle) Ewald <wolfgang.ewald@wolles-elektronikkiste.de>
2022-03-25 21:47:03 +0100

    Add files via upload

--- examples/MPU9250_SPI_all_data/MPU9250_SPI_all_data.ino
+++ examples/MPU9250_SPI_all_data/MPU9250_SPI_all_data.ino
@@ -1,1 +1,97 @@
+/***************************************************************************
+* Example sketch for the MPU9250_WE library
+*
+* This sketch shows how to get acceleration, gyroscocope, magnetometer and 
+* temperature data from the MPU9250 using SPI.
+* 
+* For further information visit my blog:
+*
+* https://wolles-elektronikkiste.de/mpu9250-9-achsen-sensormodul-teil-1  (German)
+* https://wolles-elektronikkiste.de/en/mpu9250-9-axis-sensor-module-part-1  (English)
+* 
+***************************************************************************/
 
+#include <MPU9250_WE.h>
+const int csPin = 10;  // Chip Select Pin
+bool useSPI = true;    // SPI use flag
+
+/* There is only one construictor for SPI: */
+MPU9250_WE myMPU9250 = MPU9250_WE(&SPI, csPin, useSPI);
+
+void setup() {
+  Serial.begin(115200);
+  if(!myMPU9250.init()){
+    Serial.println("MPU9250 does not respond");
+  }
+  else{
+    Serial.println("MPU9250 is connected");
+  }
+  if(!myMPU9250.initMagnetometer()){
+    Serial.println("Magnetometer does not respond");
+  }
+  else{
+    Serial.println("Magnetometer is connected");
+  }
+
+  /* Choose the SPI clock speed, default is 8 MHz 
+     This function must be used only after init(), not before */
+  //myMPU9250.setSPIClockSpeed(4000000);
+
+  Serial.println("Position you MPU9250 flat and don't move it - calibrating...");
+  delay(1000);
+  myMPU9250.autoOffsets();
+  Serial.println("Done!");
+  
+  //myMPU9250.setAccOffsets(-14240.0, 18220.0, -17280.0, 15590.0, -20930.0, 12080.0);
+  //myMPU9250.setGyrOffsets(45.0, 145.0, -105.0);
+  myMPU9250.enableGyrDLPF();
+  //myMPU9250.disableGyrDLPF(MPU9250_BW_WO_DLPF_8800); // bandwdith without DLPF
+  myMPU9250.setGyrDLPF(MPU9250_DLPF_6);
+  myMPU9250.setSampleRateDivider(5);
+  myMPU9250.setGyrRange(MPU9250_GYRO_RANGE_250);
+  myMPU9250.setAccRange(MPU9250_ACC_RANGE_2G);
+  myMPU9250.enableAccDLPF(true);
+  myMPU9250.setAccDLPF(MPU9250_DLPF_6);
+  //myMPU9250.enableAccAxes(MPU9250_ENABLE_XYZ);
+  //myMPU9250.enableGyrAxes(MPU9250_ENABLE_XYZ);
+  myMPU9250.setMagOpMode(AK8963_CONT_MODE_100HZ);
+  delay(200);
+}
+
+void loop() {
+  xyzFloat gValue = myMPU9250.getGValues();
+  xyzFloat gyr = myMPU9250.getGyrValues();
+  xyzFloat magValue = myMPU9250.getMagValues();
+  float temp = myMPU9250.getTemperature();
+  float resultantG = myMPU9250.getResultantG(gValue);
+
+  Serial.println("Acceleration in g (x,y,z):");
+  Serial.print(gValue.x);
+  Serial.print("   ");
+  Serial.print(gValue.y);
+  Serial.print("   ");
+  Serial.println(gValue.z);
+  Serial.print("Resultant g: ");
+  Serial.println(resultantG);
+
+  Serial.println("Gyroscope data in degrees/s: ");
+  Serial.print(gyr.x);
+  Serial.print("   ");
+  Serial.print(gyr.y);
+  Serial.print("   ");
+  Serial.println(gyr.z);
+
+  Serial.println("Magnetometer Data in µTesla: ");
+  Serial.print(magValue.x);
+  Serial.print("   ");
+  Serial.print(magValue.y);
+  Serial.print("   ");
+  Serial.println(magValue.z);
+
+  Serial.print("Temperature in °C: ");
+  Serial.println(temp);
+
+  Serial.println("********************************************");
+
+  delay(1000);
+}
